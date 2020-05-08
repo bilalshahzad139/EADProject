0646378474bbbd84fd3e0a04bb80be5d4758acf3
usman <bsef17m028@pucit.edu.pk>
2020-05-08 17:36:04 +0500

    column for multiple images added in product table

--- Src/DBProj/Tables/Products.sql
+++ Src/DBProj/Tables/Products.sql
@@ -2,12 +2,15 @@
     [ProductID]   INT          IDENTITY (1, 1) NOT NULL,
     [Name]        VARCHAR (50) NOT NULL,
     [Price]       FLOAT (53)   NOT NULL,
-    [PictureName] VARCHAR (50) NOT NULL,
     [CreatedOn]   DATETIME     NOT NULL,
     [CreatedBy]   INT          NOT NULL,
     [ModifiedOn]  DATETIME     NULL,
     [ModifiedBy]  INT          NULL,
     [IsActive]    BIT          NOT NULL,
+    [PictureName1] VARCHAR(50) NOT NULL, 
+    [PictureName2] VARCHAR(50) NULL, 
+    [PictureName3] VARCHAR(50) NULL, 
+    [PictureName4] VARCHAR(50) NULL, 
     CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID] ASC)
 );
 
