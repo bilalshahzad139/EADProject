9dbc506059f4926fd9a6c921c5b4108060a6b93c
usman <bsef17m028@pucit.edu.pk>
2020-05-10 01:00:11 +0500

    Table Name and Procedure name edited

--- Src/DBProj/Procedures/dbo_InsertPictureName.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[InsertPictureName]
-	@ProductID int,
-	@PictureName varchar(50)
-AS
-	INSERT INTO PictureNames(ProductID, PictureName)
-VALUES (@ProductID, @PictureName)
-
-return
--- Src/DBProj/Tables/PictureNames.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[PictureNames]
-(
-	[PictureID] INT NOT NULL IDENTITY , 
-    [ProductID] INT NOT NULL, 
-    [PictureName] VARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_PictureNames_Products] FOREIGN KEY ([ProductID]) REFERENCES [Products]([ProductID]), 
-    CONSTRAINT [PK_PictureNames] PRIMARY KEY ([PictureID])
-)
