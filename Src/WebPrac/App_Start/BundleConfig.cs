383dcd08ac5b124ebffdc7ff15b7eb168df83b05
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-05-09 22:34:41 +0500

    // created top bar with navigation bar //updated product layout

--- Src/WebPrac/App_Start/BundleConfig.cs
+++ Src/WebPrac/App_Start/BundleConfig.cs
@@ -21,8 +21,7 @@ namespace WebPrac
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
