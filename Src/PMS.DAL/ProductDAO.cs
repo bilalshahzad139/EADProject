03f17ffd7558d495edf5c5c00001ddf7b2c4bcc4
Usama Shahid <mughal.usama1@yahoo.com>
2020-05-12 20:54:51 +0500

    Resolved Query Errors

--- Src/PMS.DAL/ProductDAO.cs
+++ Src/PMS.DAL/ProductDAO.cs
@@ -18,7 +18,7 @@ namespace PMS.DAL
                 if (dto.ProductID > 0)
                 {
                     sqlQuery =
-                        $"Update dbo.Products Set Name='{dto.Name}',Price='{dto.Price}',ModifiedOn='{dto.ModifiedOn}',ModifiedBy='{dto.ModifiedBy}' Where ProductID={dto.ProductID}";
+                        $"Update dbo.Products Set Name='{dto.Name}',Price='{dto.Price}',ModifiedOn='{dto.ModifiedOn}',ModifiedBy='{dto.ModifiedBy}' Where ProductID={dto.ProductID}; Update dbo.ProductPictureNames Set PictureName = '{dto.PictureName}' where ProductID = '{dto.ProductID}'";
                     helper.ExecuteQuery(sqlQuery);
                     return dto.ProductID;
                 }
@@ -28,13 +28,17 @@ namespace PMS.DAL
                         $"INSERT INTO dbo.Products(Name, Price, CreatedOn, CreatedBy,IsActive,ProductCategoryID) VALUES('{dto.Name}','{dto.Price}','{dto.CreatedOn}','{dto.CreatedBy}',{1},'1'); Select @@IDENTITY";
 
                     var obj = helper.ExecuteScalar(sqlQuery);
+                    sqlQuery =
+                        $"INSERT INTO dbo.ProductPictureNames(PictureName, ProductID ) Values ('{dto.PictureName}','{Convert.ToInt32(obj)}');";
+                    helper.ExecuteQuery(sqlQuery);
                     return Convert.ToInt32(obj);
                 }
             }
         }
         public static ProductDTO GetProductById(int pid)
         {
-            var query = $"Select * from dbo.Products Where ProductId={pid}";
+            var query = $"Select a.ProductID, a.Name, a.Price, a.CreatedBy, a.CreatedOn, a.ModifiedBy, a.ModifiedOn, a.ProductCategoryID, a.IsActive, b.PictureName from dbo.Products a full outer join dbo.ProductPictureNames b on a.ProductID = b.ProductID where a.ProductID='{pid}'";
+            ;
 
             using (var helper = new DBHelper())
             {
@@ -50,7 +54,7 @@ namespace PMS.DAL
 
         public static List<ProductDTO> GetAllProducts(bool pLoadComments=false)
         {
-            const string query = "Select * from dbo.Products Where IsActive = 1;";
+            const string query = "Select a.ProductID, a.Name, a.Price, a.CreatedBy, a.CreatedOn, a.ModifiedBy, a.ModifiedOn, a.ProductCategoryID, a.IsActive, b.PictureName from dbo.Products a full outer join dbo.ProductPictureNames b on a.ProductID = b.ProductID where a.IsActive = 1;";
 
             using (var helper = new DBHelper())
             {
@@ -118,18 +122,18 @@ namespace PMS.DAL
         private static ProductDTO FillDTO(SqlDataReader reader)
         {
             var dto = new ProductDTO();
-            dto.ProductID = reader.GetInt32(0);
-            dto.Name = reader.GetString(1);
-            dto.Price = reader.GetDouble(2);
-            dto.PictureName = reader.GetString(3);
-            dto.CreatedOn = reader.GetDateTime(4);
-            dto.CreatedBy = reader.GetInt32(5);
-            if (reader.GetValue(6) != DBNull.Value)
-                dto.ModifiedOn = reader.GetDateTime(6);
-            if (reader.GetValue(7) != DBNull.Value)
-                dto.ModifiedBy = reader.GetInt32(7);
+            dto.ProductID = reader.GetInt32(reader.GetOrdinal("ProductID"));
+            dto.Name = reader.GetString(reader.GetOrdinal("Name"));
+            dto.Price = reader.GetDouble(reader.GetOrdinal("Price"));
+            dto.PictureName = reader.GetString(reader.GetOrdinal("PictureName"));
+            dto.CreatedOn = reader.GetDateTime(reader.GetOrdinal("CreatedOn"));
+            dto.CreatedBy = reader.GetInt32(reader.GetOrdinal("CreatedBy"));
+            if (reader.GetValue(reader.GetOrdinal("ModifiedOn")) != DBNull.Value)
+                dto.ModifiedOn = reader.GetDateTime(reader.GetOrdinal("ModifiedOn"));
+            if (reader.GetValue(reader.GetOrdinal("ModifiedBy")) != DBNull.Value)
+                dto.ModifiedBy = reader.GetInt32(reader.GetOrdinal("ModifiedBy"));
 
-            dto.IsActive = reader.GetBoolean(8);
+            dto.IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"));
             return dto;
         }
     }
