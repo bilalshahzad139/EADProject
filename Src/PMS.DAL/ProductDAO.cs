38f81247fdde351477c14aa3321152b2bf96c4d7
Alishba-Saeed <alishbasaeed7109@gmail.com>
2020-05-12 17:34:31 -0700

    'ProductSearchDAO added'

--- Src/PMS.DAL/ProductDAO.cs
+++ Src/PMS.DAL/ProductDAO.cs
@@ -104,41 +104,6 @@ namespace PMS.DAL
                 return list;
             }
         }
-        public static List<ProductDTO> GetProductByName(String prodName)
-        {
-            var query = "SP_Product_SearchByName";
-            using (var helper = new DBHelper())
-            {
-                SqlConnection _conn = helper.ReturnConnection();
-                if (_conn == null)
-                {
-                    return null;
-                }
-                SqlCommand command = new SqlCommand(query,_conn);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                SqlParameter param = new SqlParameter();
-                param.ParameterName = "product_name";
-                param.SqlDbType = System.Data.SqlDbType.VarChar;
-                param.Value = prodName;
-                command.Parameters.Add(param);
-                var reader = command.ExecuteReader();
-                var list = new List<ProductDTO>();
-
-                while (reader.Read())
-                {
-                    var dto = FillDTO(reader);
-                    if (dto != null) list.Add(dto);
-                }
-                var commentsList = CommentDAO.GetTopComments(2);
-
-                foreach (var prod in list)
-                {
-                    var prodComments = commentsList.Where(c => c.ProductID == prod.ProductID).ToList();
-                    prod.Comments = prodComments;
-                }
-                return list;
-            }
-        }
         public static int DeleteProduct(int pid)
         {
             var sqlQuery = $"Update dbo.Products Set IsActive=0 Where ProductID={pid}";
---
+++ Src/PMS.DAL/ProductSearchDAO.cs
@@ -0,0 +1,66 @@
+﻿using PMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.DAL
+{
+    public class ProductSearchDAO
+    {
+        private static ProductDTO FillDTO(SqlDataReader reader)
+        {
+            var dto = new ProductDTO();
+            dto.ProductID = reader.GetInt32(0);
+            dto.Name = reader.GetString(1);
+            dto.Price = reader.GetDouble(2);
+            dto.PictureName = reader.GetString(3);
+            dto.CreatedOn = reader.GetDateTime(4);
+            dto.CreatedBy = reader.GetInt32(5);
+            if (reader.GetValue(6) != DBNull.Value)
+                dto.ModifiedOn = reader.GetDateTime(6);
+            if (reader.GetValue(7) != DBNull.Value)
+                dto.ModifiedBy = reader.GetInt32(7);
+
+            dto.IsActive = reader.GetBoolean(8);
+            return dto;
+        }
+        public static List<ProductDTO> GetProductByName(String prodName)
+        {
+            var query = "SP_Product_SearchByName";
+            using (var helper = new DBHelper())
+            {
+                SqlConnection _conn = helper.ReturnConnection();
+                if (_conn == null)
+                {
+                    return null;
+                }
+                SqlCommand command = new SqlCommand(query, _conn);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "product_name";
+                param.SqlDbType = System.Data.SqlDbType.VarChar;
+                param.Value = prodName;
+                command.Parameters.Add(param);
+                var reader = command.ExecuteReader();
+                var list = new List<ProductDTO>();
+
+                while (reader.Read())
+                {
+                    var dto = FillDTO(reader);
+                    if (dto != null) list.Add(dto);
+                }
+                var commentsList = CommentDAO.GetTopComments(2);
+
+                foreach (var prod in list)
+                {
+                    var prodComments = commentsList.Where(c => c.ProductID == prod.ProductID).ToList();
+                    prod.Comments = prodComments;
+                }
+                return list;
+            }
+        }
+    }
+}
