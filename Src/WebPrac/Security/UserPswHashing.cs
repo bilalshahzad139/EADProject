a3fc64cacf4f4166b9338b6647f49b0b99b669a0
shisui <bsef17m051@pucit.edu.pk>
2020-05-15 17:37:59 +0500

    User Passwor Hashing class added in WebPrac.Security Folder

---
+++ Src/WebPrac/Security/UserPswHashing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebPrac.Security
+{
+    public static class UserPswHashing
+    {
+        public static string CreateSalt(int size)
+        {
+            //Generate a cryptographic random number.
+            var rng = new RNGCryptoServiceProvider();
+            var buff = new byte[size];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public static string GenerateHash(string input, string salt)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input + salt);
+            var sHA256ManagedString = new SHA256Managed();
+            var hash = sHA256ManagedString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool AreEqual(string plainTextInput, string hashedInput, string salt)
+        {
+            var newHashedPin = GenerateHash(plainTextInput, salt);
+            return newHashedPin.Equals(hashedInput);
+        }
+    }
+}
