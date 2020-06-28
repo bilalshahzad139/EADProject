f735412fbd4a80d25c1f30b8b7ec9744a0a772f0
usmanmurad <bsef17m021@pucit.edu.pk>
2020-06-28 12:31:27 +0500

    query string updated to include isAdmin and isActive

--- Src/DBProj/Tables/ProductCategory.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[ProductCategory]
-(
-	[ProductCategoryID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProductCategoryName] VARCHAR(100) NOT NULL 
-)
--- Src/DBProj/Tables/dbo.Distributors.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Distributors]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Country] VARCHAR(50) NOT NULL, 
-    [Address] VARCHAR(MAX) NOT NULL, 
-    [Phone] VARCHAR(50) NOT NULL, 
-    [Website] VARCHAR(50) NULL
-);
