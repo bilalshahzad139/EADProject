1b94382bd99c6183c75e76d5e048834dcaf0a12c
Nadia <nadiaahsan956@gmail.com>
2020-06-30 14:41:41 +0500

    Product Info Table added

---
+++ Src/DBProj/Tables/dbo.ProductSoldInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ProductSoldInfo]
+(
+	[ProductID] INT NOT NULL, 
+    [SoldQuantity] INT NOT NULL  , 
+    [SoldDate] DATE NOT NULL,
+    PRIMARY KEY ([ProductID],[SoldDate]),
+    CONSTRAINT [FKProductId]  FOREIGN KEY (ProductID) REFERENCES Products([ProductID]),
+
+
+ )
+    
